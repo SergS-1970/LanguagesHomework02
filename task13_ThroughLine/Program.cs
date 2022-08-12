15e97168f8691d221567e50fa205963624fda1c9
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-08-13 02:16:20 +0300

    task13 String

---
+++ task13_ThroughLine/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите натуральное число N: ");
+string numberN = Console.ReadLine();
+
+int lenghtString = numberN.Length;
+
+if (lenghtString < 3)
+{
+   Console.WriteLine("Число N двузначное; третьей цифры нет"); 
+}
+
+else 
+{
+    Console.WriteLine("Третья цифра числа N = " + numberN[2]);
+}
